5325d29bf9557ed9667c2bede14ce01c31c453a7
pxgamer <owzie123@gmail.com>
2017-10-19 12:08:47 +0100

    Updated test.php to use the autoloader

--- test.php
+++ test.php
@@ -1,21 +1,20 @@
 <?php
 
-require_once './includes.inc.php';
+use Ethereum\EthBlockParam;
+use Ethereum\EthD32;
+use Ethereum\EthQ;
+use Ethereum\Filter;
+use Ethereum\Transaction;
 
-require_once './src/EthFilter.generated.php';
-require_once './src/EthTransaction.generated.php';
+require_once 'vendor/autoload.php';
+
+$quantity = new EthQ('0x0000000000000000000000000000001');
+printMe ('Setting and getting 1 at EthQ()', $quantity->hexVal());
 
 
-$quantity = new EthQ();
-$quantity->set('0x0000000000000000000000000000001');
-printMe ('Setting and getting 1 at EthQ()', $quantity->getHexVal());
-
-
-$block = new EthBlockParam();
-$quantity->set('0xe4c2c112df6f03cd629ab5e66792b7a4b86a64c3971c0e24764d3e9f68f7cd64');
+$block = new EthBlockParam('0xe4c2c112df6f03cd629ab5e66792b7a4b86a64c3971c0e24764d3e9f68f7cd64');
 
 $filter = new Filter($block);
-$filter->set($block, 'fromBlock');
 
 // This should throw an InvalidArgumentException:
 // $X->set('fromBlock', 'hello');
@@ -23,8 +22,7 @@ $filter->set($block, 'fromBlock');
 printMe ('Filter toArray()', $filter->toArray());
 
 
-$hash = new EthD32();
-$hash->set('0xe4c2c112df6f03cd629ab5e66792b7a4b86a64c3971c0e24764d3e9f68f7cd64');
+$hash = new EthD32('0xe4c2c112df6f03cd629ab5e66792b7a4b86a64c3971c0e24764d3e9f68f7cd64');
 $tx = new Transaction($hash);
 
 printMe ('Transaction toArray()', $tx->toArray());
