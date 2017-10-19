623df4a37c8518ed9f0679ab2b7cf5e98832de10
pxgamer <owzie123@gmail.com>
2017-10-19 12:16:17 +0100

    Updated ethereumTester to use autoloading

--- ethereumTester.php
+++ ethereumTester.php
@@ -1,27 +1,25 @@
 <?php
 
+use Ethereum\Ethereum;
 
-require_once './includes.inc.php';
+require_once 'vendor/autoload.php';
 
+$schema = json_decode(file_get_contents("./ethjs-schema.json"), true);
 
-$foo = new Ethereum;
+$foo = new Ethereum('http://localhost:8545');
 
 // $params[0] = Constructor types.
 // $params[1] = Return types.
 foreach ($schema['methods'] as $fn_name => $params) {
+    echo "<h3>" . $fn_name . "</h3>";
 
-  echo "<h3>" .  $fn_name ."</h3>";
-
-  if (count($params[0])) {
-    echo "Arguments: <pre>" . print_r($params[0], true) . "</pre>";
-    call_user_func_array(array($foo, $fn_name), $params[0]);
-  }
-  else {
-    $foo->{$fn_name}();
-  }
-  echo "<hr />";
-
-
+    if (count($params[0])) {
+        echo "Arguments: <pre>" . print_r($params[0], true) . "</pre>";
+        call_user_func_array([$foo, $fn_name], $params[0]);
+    } else {
+        $foo->{$fn_name}();
+    }
+    echo "<hr />";
 }
 
 // echo "<h1>SCHEMA</h1>";
