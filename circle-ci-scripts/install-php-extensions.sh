d8639e52de9c6d222971bcdb038bbeb68f7c3847
Thorsten Krug <tho@donkeymedia.eu>
2021-06-03 15:23:09 +0200

    Update

--- circle-ci-scripts/install-php-extensions.sh
+++ circle-ci-scripts/install-php-extensions.sh
@@ -1,4 +1,4 @@
-#!/bin.sh
+#!/bin/sh
 sudo pecl install mcrypt-1.0.4 && \
 sudo docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/ && \
 sudo docker-php-ext-install -j$(nproc) gd && \
