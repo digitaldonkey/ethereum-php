e0a81a037c86cbcca0de012bdee1e1098b4cf488
Thorsten Krug <tho@donkeymedia.eu>
2021-06-03 15:12:38 +0200

    Update extension script

--- circle-ci-scripts/install-php-extensions.sh
+++ circle-ci-scripts/install-php-extensions.sh
@@ -1,8 +1,6 @@
 #!/bin.sh
 sudo pecl install mcrypt-1.0.4 && \
-sudo docker-php-ext-enable mcrypt && \
 sudo docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/ && \
 sudo docker-php-ext-install -j$(nproc) gd && \
 sudo docker-php-ext-configure gmp  && \
-sudo docker-php-ext-install gmp  && \
-sudo docker-php-ext-install zip
+sudo docker-php-ext-install gmp
