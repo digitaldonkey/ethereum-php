f378657504cfb9c47890e5043f677d830038f2f3
Thorsten Krug <tho@donkeymedia.eu>
2021-06-14 00:42:29 +0200

    Update config

--- .circleci/scripts/install-php-extensions.sh
+++ .circleci/scripts/install-php-extensions.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#sudo pecl install mcrypt-1.0.4 && \
-sudo docker-php-ext-install -j$(nproc) mcrypt && \
+sudo pecl install mcrypt-1.0.4 && \
 sudo docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/ && \
 sudo docker-php-ext-configure gmp  && \
 sudo docker-php-ext-install -j$(nproc) gmp gd && \
