3bf69b002dc7ad34c218939ac47dd0aabd1e4a48
Thorsten Krug <tho@donkeymedia.eu>
2021-06-14 16:17:19 +0200

    Fix merged test

--- tests/EthMainnet/Unit/FixUnpaddedValuesTest.php
+++ tests/EthMainnet/Unit/FixUnpaddedValuesTest.php
@@ -14,6 +14,7 @@ class FixUnpaddedValuesTest extends TestEthMainnet
 {
 
     public function testMainNetTx() {
+        // @see https://etherscan.io/tx/0x527e9b670536c09fe160b0c2e33728eadd7f8affae7803049fa8d8ec2d1c7cfe
         $mainNetTx = new EthD32('0x527e9b670536c09fe160b0c2e33728eadd7f8affae7803049fa8d8ec2d1c7cfe');
         $transaction = $this->web3->eth_getTransactionByHash($mainNetTx);
         $this->assertSame(1.12208103, EthereumStatic::convertCurrency($transaction->value->val()));
@@ -26,11 +27,9 @@ class FixUnpaddedValuesTest extends TestEthMainnet
    *
    */
     public function testMainNetTxReceipt() {
-        $web3 = new Ethereum('https://mainnet.infura.io/drupal');
-
         // @see https://etherscan.io/tx/0x527e9b670536c09fe160b0c2e33728eadd7f8affae7803049fa8d8ec2d1c7cfe
         $mainNetTx = new EthD32('0xee04d9e2258909ca642acde0612597732f4ba244952e23eb83ecac3fe8dd5091');
-        $transaction = $web3->eth_getTransactionReceipt($mainNetTx);
+        $transaction = $this->web3->eth_getTransactionReceipt($mainNetTx);
         $this->asserttrue(is_null($transaction->logs[0]->data));
     }
 }
